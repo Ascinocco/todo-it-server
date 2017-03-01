713d60e1d08a664c6a708b48335679cd13388328
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-01 17:15:35 -0500

    changed routes to constants

--- Server.ts
+++ Server.ts
@@ -7,8 +7,8 @@ import * as bodyParser from "body-parser";
 import * as cookieParser from "cookie-parser";
 
 import { CookieParserConfig } from "./api/config/CookieParserConfig";
-let AuthRoutes = require("./api/Routes/AuthRoutes");
-let UserRoutes = require("./api/Routes/UserRoutes");
+const AuthRoutes = require("./api/Routes/AuthRoutes");
+const UserRoutes = require("./api/Routes/UserRoutes");
 
 
 export class Server
