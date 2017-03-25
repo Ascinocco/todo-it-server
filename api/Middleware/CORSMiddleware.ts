1fa3f043828e105a3ce796a64a66c27a01b3ea23
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-25 18:05:08 -0400

    no more development

--- api/Middleware/CORSMiddleware.ts
+++ api/Middleware/CORSMiddleware.ts
@@ -6,19 +6,19 @@ export class CORSMiddleware
 
     /**
      * Configures CORS so that my angular app can access server
-     * 
-     * 
+     *
+     *
      * @static
-     * @param {Request} req 
-     * @param {Response} res 
-     * @param {NextFunction} next 
-     * 
+     * @param {Request} req
+     * @param {Response} res
+     * @param {NextFunction} next
+     *
      * @memberOf CORSMiddleware
      */
     public static allowCORS(req: Request, res: Response, next: NextFunction): void
     {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
         res.header('Access-Control-Expose-Headers', 'Connection,Content-Length,Content-Type,Date,ETag,X-Powered-By,x-access-token');
         res.header('Access-Control-Allow-Headers', 'Connection,Content-Length,Content-Type,Date,ETag,X-Powered-By,x-access-token');
         next();
