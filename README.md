cbf4f4cca27285af5e9d4a21932e4f3c36c65d0b
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-25 23:23:51 -0400

    Revert "no more development"
    This reverts commit 1fa3f043828e105a3ce796a64a66c27a01b3ea23.

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-# NO LONGER MAINTAINED
-In the past couple months I've learned quite a few things about angular 2 and Laravel 
-and I want to begin a rewrite. This project is incomplete and will remain incomplete.
-
-Please view the todo-it-2-cleint repo
-and todo-it-2-server repo
-
 # TodoIt-Server
 
 This is the server side code for my TodoIt app
--- api/Middleware/CORSMiddleware.js
+++ api/Middleware/CORSMiddleware.js
@@ -5,7 +5,7 @@ var CORSMiddleware = (function () {
     }
     CORSMiddleware.allowCORS = function (req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
         res.header('Access-Control-Expose-Headers', 'Connection,Content-Length,Content-Type,Date,ETag,X-Powered-By,x-access-token');
         res.header('Access-Control-Allow-Headers', 'Connection,Content-Length,Content-Type,Date,ETag,X-Powered-By,x-access-token');
         next();
