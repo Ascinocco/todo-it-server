b8af557452b996c53fcd157791776620ab89fda6
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-27 11:48:05 -0400

    refactored valid alerts

--- api/Models/Todo/Constants/ValidAlerts.ts
+++ api/Models/Todo/Constants/ValidAlerts.ts
@@ -1,87 +1,42 @@
 export const ValidAlerts: Object = {
-    ZERO_MIN: {
-        value: 0,
-        measure: 'min'
-    },
-    TEN_MIN: {
-        value: 10,
-        measure: 'min'
-    },
-    FIFTEEN_MIN: {
-        value: 15,
-        measure: 'min'
-    },
-    THIRTY_MIN: {
-        value: 30,
-        measure: 'min'
-    },
-    FORTY_FIVE_MIN: {
-        value: 45,
-        measure: 'min'
+
+    MIN: {
+        unit: 'min',
+        ZERO: 0,
+        TEN: 10,
+        FIFTEEN: 15,
+        THIRTY: 30,
+        FORTY_FIVE: 45
     },
 
-    ONE_HOUR: {
-        value: 1,
-        measure: 'hour'
-    },
-    TWO_HOUR: {
-        value: 2,
-        measure: 'hour'
-    },
-    THREE_HOUR: {
-        value: 3,
-        measure: 'hour'
-    },
-    FOUR_HOUR: {
-        value: 4,
-        measure: 'hour'
-    },
-    FIVE_HOUR: {
-        value: 5,
-        measure: 'hour'
-    },
-    SIX_HOUR: {
-        value: 6,
-        measure: 'hour'
+    HOUR: {
+        unit: 'hour',
+        ONE: 1,
+        TWO: 2,
+        THREE: 3,
+        FOUR: 4,
+        FIVE: 5,
+        SIX: 6
     },
 
-    ONE_DAY: {
-        value: 1,
-        measure: 'day'
-    },
-    TWO_DAY: {
-        value: 2,
-        measure: 'day'
-    },
-    THREE_DAY: {
-        value: 3,
-        measure: 'day'
-    },
-    FOUR_DAY: {
-        value: 4,
-        measure: 'day'
-    },
-    FIVE_DAY: {
-        value: 5,
-        measure: 'day'
-    },
-    SIX_DAY: {
-        value: 6,
-        measure: 'day'
+    DAY: {
+        unit: 'day',
+        ONE: 1,
+        TWO: 2,
+        THREE: 3,
+        FOUR: 4,
+        FIVE: 5,
+        SIX: 6,
     },
 
-    ONE_WEEK: {
-        value: 1,
-        measure: 'week'
-    },
-    TWO_WEEK: {
-        value: 2,
-        measure: 'week'
+    WEEK: {
+        unit: 'week',
+        ONE: 1,
+        TWO: 2,
     },
 
-    ONE_MONTH: {
-        value: 1,
-        measure: 'month'
+    MONTH: {
+        unit: 'month',
+        ONE: 1,
     }
-
 };
