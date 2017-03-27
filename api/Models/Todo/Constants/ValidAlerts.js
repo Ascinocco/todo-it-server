bff89cb667e03efd43d5bfad19c276b301106512
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-27 11:48:09 -0400

    map and js

--- api/Models/Todo/Constants/ValidAlerts.js
+++ api/Models/Todo/Constants/ValidAlerts.js
@@ -1,85 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ValidAlerts = {
-    ZERO_MIN: {
-        value: 0,
-        measure: 'min'
+    MIN: {
+        unit: 'min',
+        ZERO: 0,
+        TEN: 10,
+        FIFTEEN: 15,
+        THIRTY: 30,
+        FORTY_FIVE: 45
     },
-    TEN_MIN: {
-        value: 10,
-        measure: 'min'
+    HOUR: {
+        unit: 'hour',
+        ONE: 1,
+        TWO: 2,
+        THREE: 3,
+        FOUR: 4,
+        FIVE: 5,
+        SIX: 6
     },
-    FIFTEEN_MIN: {
-        value: 15,
-        measure: 'min'
+    DAY: {
+        unit: 'day',
+        ONE: 1,
+        TWO: 2,
+        THREE: 3,
+        FOUR: 4,
+        FIVE: 5,
+        SIX: 6,
     },
-    THIRTY_MIN: {
-        value: 30,
-        measure: 'min'
+    WEEK: {
+        unit: 'week',
+        ONE: 1,
+        TWO: 2,
     },
-    FORTY_FIVE_MIN: {
-        value: 45,
-        measure: 'min'
-    },
-    ONE_HOUR: {
-        value: 1,
-        measure: 'hour'
-    },
-    TWO_HOUR: {
-        value: 2,
-        measure: 'hour'
-    },
-    THREE_HOUR: {
-        value: 3,
-        measure: 'hour'
-    },
-    FOUR_HOUR: {
-        value: 4,
-        measure: 'hour'
-    },
-    FIVE_HOUR: {
-        value: 5,
-        measure: 'hour'
-    },
-    SIX_HOUR: {
-        value: 6,
-        measure: 'hour'
-    },
-    ONE_DAY: {
-        value: 1,
-        measure: 'day'
-    },
-    TWO_DAY: {
-        value: 2,
-        measure: 'day'
-    },
-    THREE_DAY: {
-        value: 3,
-        measure: 'day'
-    },
-    FOUR_DAY: {
-        value: 4,
-        measure: 'day'
-    },
-    FIVE_DAY: {
-        value: 5,
-        measure: 'day'
-    },
-    SIX_DAY: {
-        value: 6,
-        measure: 'day'
-    },
-    ONE_WEEK: {
-        value: 1,
-        measure: 'week'
-    },
-    TWO_WEEK: {
-        value: 2,
-        measure: 'week'
-    },
-    ONE_MONTH: {
-        value: 1,
-        measure: 'month'
+    MONTH: {
+        unit: 'month',
+        ONE: 1,
     }
 };
 //# sourceMappingURL=ValidAlerts.js.map
